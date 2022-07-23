6a04484556d0de19a34e0f3e534022c268c3f40c
julia <65985506+campanula@users.noreply.github.com>
2022-07-23 14:39:50 +0100

    Added society info command and changed some variables

---
+++ src/commands/printSocietyInfo.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('joinus')
+        .setDescription('All the links and info for Essex GDS!'),
+    async execute(interaction, client) {
+        console.log('Print info');
+
+        const userEmbed = new MessageEmbed()
+            .setTitle('Essex Game Dev Society')
+            .setDescription('All the info and links you need :)')
+            .addFields(
+                {name: 'Join  the society!', value: 'Join here: https://www.essexstudent.com/society/gamedevsociety/ ', inline: true},
+                {name: 'Linktree', value: 'Find all our other socials: https://linktr.ee/essexGDS ', inline: true}
+            )
+            .setURL('https://github.com/campanula/game-dev-soc-bot')
+            .setColor('BLURPLE')
+            .setTimestamp()
+            .setFooter({
+                text: `Triggered by ${interaction.user.tag}`
+                })
+
+        await interaction.reply({ embeds: [userEmbed] });
+    }
+}
