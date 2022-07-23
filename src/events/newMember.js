bf851ae308daa5811694528b2467aba353db28d5
julia <65985506+campanula@users.noreply.github.com>
2022-07-23 18:44:50 +0100

    Fixed welcome command
    - Message will now send w/o breaking

--- src/events/newMember.js
+++ src/events/newMember.js
@@ -3,23 +3,23 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'guildMemberAdd',
-    async execute(client, member) {
+    async execute(member) {
+		console.log(member.user);
+        let welcomeChannel = member.guild.channels.cache.get(channelId)
+        welcomeChannel.send(`${member.user} has joined the server!`);
 
         const welcomeEmbed = new MessageEmbed()
-            .setTitle(`Welcome to ${member.user.username}!`)
+            .setTitle(`Welcome, ${member.user.username}!`)
             .setDescription('Thanks for joining!')
             .addFields(
                 {name: 'Not a society member?', value: 'You can join for free here! \nhttps://www.essexstudent.com/society/gamedevsociety/ '},
                 {name: 'Joined and looking for that cool member role? 👀', value: 'Message one of the executives with your PRID and name!'},
-                {name: 'Roles', value: 'Make sure to pick up your other roles in #🎓roles for access to exclusive channels!'},
+                {name: 'Need some roles?', value: 'Make sure to pick up your other roles in #🎓roles for access to exclusive channels!'},
                 {name: 'Need game dev help?', value: 'Check out #🙌game-dev-help or #📚game-dev-resources'}
             )
             .setColor('BLURPLE')
             .setTimestamp()
         
-        const messageChannel = member.guild.channels.cache.get(channelId);
-        member.guild.channels.cache.get(channelId).send(`${member.user.username} has joined the server!`);
-        console.log(`${member.user.username} has joined the server!`);
-        messageChannel.send({ embeds: [welcomeEmbed] });
-    }
+        welcomeChannel.send({ embeds: [welcomeEmbed] });
+	}
 }
