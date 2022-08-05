221c4a3bec6ee9c6474b0aef20657f64320f729d
julia <65985506+campanula@users.noreply.github.com>
2022-08-05 13:25:24 +0100

    Added command to clear submissions

---
+++ src/commands/jam-submissions/clearSubmissions.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const { read, write } = require("../../saveArray.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("clear-submissions")
+        .setDescription("Clears submission list - admin only"), //command perms currently controlled in server settings not here
+    defaultPermission: false,
+    async execute(interaction) {
+        submissions = read("src/txt/submissions.txt");
+        if (submissions.length === 0) {
+            await interaction.reply({
+                content: "The list is already empty!",
+                ephemeral: true
+            });
+        } else {
+
+            console.log("Clear sublist attempt");
+            submissions.length = 0;
+            write(submissions, "src/txt/submissions.txt");
+
+            const clear_Embed = new MessageEmbed()
+                .setDescription("List cleared\nThere are now " + submissions.length + " submissions in the list.")
+                .setColor("BLURPLE")
+                .setTimestamp()
+                .setFooter({
+                    text: `Triggered by ${interaction.user.tag}`
+                })
+
+
+            await interaction.reply({ embeds: [clear_Embed] });
+            console.log(submissions);
+        }
+    }
+}
