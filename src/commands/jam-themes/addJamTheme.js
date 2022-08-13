c680c1721b69f151c1e5151b8799171044fcd525
julia <65985506+campanula@users.noreply.github.com>
2022-08-13 19:02:20 +0100

    Updated add-theme to only add unique values to array

--- src/commands/jam-themes/addJamTheme.js
+++ src/commands/jam-themes/addJamTheme.js
@@ -12,22 +12,26 @@ module.exports = {
     async execute(interaction, client) {
         client.log.interinfo(`${interaction.user.tag} used the /add-theme command in #${interaction.channel.name}`);
 
-        const value = interaction.options.getString("input");
+        const value = interaction.options.getString("input").toLowerCase(); // convert to lower case for comparison
 
         const theme = read("src/txt/themes.txt");
-        theme.push(value);
+        if (!theme.includes(value)) {
+            theme.push(value);
 
-        write(theme, "src/txt/themes.txt");
+            write(theme, "src/txt/themes.txt");
 
-        const add_Embed = new EmbedBuilder()
-            .setDescription(`Theme ${value} added to list\nThere are now ${theme.length} themes in the list.`)
-            .setColor("#5865F2")
-            .setTimestamp()
-            .setFooter({
-                text: `Triggered by ${interaction.user.tag}`
-            })
+            const add_Embed = new EmbedBuilder()
+                .setDescription(`Theme ${value} added to list\nThere are now ${theme.length} themes in the list.`)
+                .setColor("#5865F2")
+                .setTimestamp()
+                .setFooter({
+                    text: `Triggered by ${interaction.user.tag}`
+                })
 
 
-        await interaction.reply({ embeds: [add_Embed] });
+            await interaction.reply({ embeds: [add_Embed] });
+        } else {
+            await interaction.reply({ content: "Theme already exists in list.", ephemeral: true });
+        }
     }
 }
