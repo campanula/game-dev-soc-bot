fe7ea6bef48fada98af7ffe9dd0d4b84e70d58e9
julia <65985506+campanula@users.noreply.github.com>
2022-08-19 19:40:48 +0100

    Refactor: signup_modal.js

--- src/components/modals/signup_modal.js
+++ src/components/modals/signup_modal.js
@@ -5,44 +5,49 @@ module.exports = {
         name: "signup_modal",
     },
     async execute(interaction, client) {
-
-        client.log.interinfo(`${interaction.user.tag} submitted the sign-up form in #${interaction.channel.name}`);
+        client.log.interinfo(
+            `${interaction.user.tag} submitted the sign-up form in #${interaction.channel.name}`
+        );
 
         // Get the data entered by the user
-        const modalName = interaction.fields.getTextInputValue('modalNameInput');
-        const modalEmail = interaction.fields.getTextInputValue('modalEmailInput');
-        const modalGithub = interaction.fields.getTextInputValue('modalGithubInput');
-        const modalExp = interaction.fields.getTextInputValue('modalExpInput');
+        const modalName = interaction.fields.getTextInputValue("modalNameInput");
+        const modalEmail = interaction.fields.getTextInputValue("modalEmailInput");
+        const modalGithub = interaction.fields.getTextInputValue("modalGithubInput");
+        const modalExp = interaction.fields.getTextInputValue("modalExpInput");
 
-        client.log.interinfo(`Form input: ${modalName}, ${modalEmail}, ${modalGithub}, ${modalExp}`);
+        client.log.interinfo(
+            `Form input: ${modalName}, ${modalEmail}, ${modalGithub}, ${modalExp}`
+        );
 
-        // Create dict with questions linked to answers
-        const signup_modal_dict = {};
+        // Create object with questions linked to answers
+        const saveCurrentInput = () => {
+            const signup_modal_dict = {};
 
-        const Questions = [
-            "Names",
-            "Emails",
-            "Githubs",
-            "Exp"
-        ];
+            const Questions = ["Names", "Emails", "Githubs", "Exp"];
 
-        const Answers = [
-            modalName,
-            modalEmail,
-            modalGithub,
-            modalExp
-        ];
+            const Answers = [modalName, modalEmail, modalGithub, modalExp];
 
-        for (let i = 0; i < Questions.length; i++) {
-            signup_modal_dict[Questions[i]] = Answers[i];
-        }
+            for (let i = 0; i < Questions.length; i++) {
+                signup_modal_dict[Questions[i]] = Answers[i];
+            }
+
+            return signup_modal_dict;
+        };
 
         // add new Q/A object to array
-        const signup_modal_array = read("src/txt/jam-misc/signupForm.txt");
-        signup_modal_array.push(signup_modal_dict);
-        write(signup_modal_array, "src/txt/jam-misc/signupForm.txt");
+        const addInputToArray = () => {
+            const signup_modal_array = read("src/txt/jam-misc/signupForm.txt");
+            signup_modal_array.push(saveCurrentInput());
 
-        await interaction.reply({ content: 'Your submission was received successfully!\n Make sure to pick up the game jammer role in #roles so you can see the relevant channels in our discord!', ephemeral: true });
+            return signup_modal_array;
+        };
 
-    }
+        write(addInputToArray(), "src/txt/jam-misc/signupForm.txt");
+
+        await interaction.reply({
+            content:
+                "Your submission was received successfully!\n Make sure to pick up the game jammer role in #roles so you can see the relevant channels in our discord!",
+            ephemeral: true,
+        });
+    },
 };
