41d2b2b93215a87f30fca301b85d61d975151c0c
julia <65985506+campanula@users.noreply.github.com>
2022-08-19 17:15:35 +0100

    Refactor: translate.js
    - Refactored code for easier testing and readability

--- src/commands/fun/translate.js
+++ src/commands/fun/translate.js
@@ -23,26 +23,33 @@ module.exports = {
         client.log.interinfo(`${interaction.user.tag} used the /translate command in #${interaction.channel.name}`);
 
         const text = interaction.options.getString("text"); // Get the text to be translated from user input
+        const origin = interaction.options.getString("origin"); // Get the languages to be used from user input
+        const target = interaction.options.getString("target");
 
-        // Get the languages to be used from user input
         // Use getCode to convert the input languages into their ISO 639-1 codes
-        const origin = getCode(interaction.options.getString("origin"));
-        const target = getCode(interaction.options.getString("target"));
+        const isoOrigin = getCode(interaction.options.getString("origin"));
+        const isoTarget = getCode(interaction.options.getString("target"));
 
-        if (origin === false || target === false) { // If either the origin or target language doesn't return an ISO 639-1 code
-            await interaction.reply({ content: "One or both of the languages you entered are not supported.\n Please see https://github.com/campanula/game-dev-soc-bot/blob/main/src/misc/languages.js for supported languages.", ephemeral: true });
+        if (isoOrigin === false || isoTarget === false) { // If either the origin or target language doesn't return an ISO 639-1 code
+            await interaction.reply({ content: "One or both of the languages you entered are not supported.\n Please see https://github.com/campanula/game-dev-soc-bot/blob/main/src/functions/misc-functions/languages.js for supported languages.", ephemeral: true });
         } else {
             try {
-                // Translate the text from origin to target language using autocorrect
-                const res = await translate(text, { from: origin, to: target, autoCorrect: true });
+                const getTranslation = async () => {
+                    // Translate the text from origin to target language using autocorrect
+                    const res = await translate(text, { from: isoOrigin, to: isoTarget, autoCorrect: true });
 
-                // If autocorrected text exists return it, else return normal text
-                let input = null;
-                res.from.text.value.length === 0 ? input = text : input = res.from.text.value;
+                    // If autocorrected text exists return it, else return normal text
+                    let input = null;
+                    res.from.text.value.length === 0 ? input = text : input = res.from.text.value;
+
+                    return [input, res];
+                }
+
+                const [input, res] = await getTranslation();
 
                 const translation_Embed = new EmbedBuilder()
                     .setTitle("Translation")
-                    .setDescription(`Translating ${input} from ${res.from.language.iso}...\n\nResult: ${res.text}`)
+                    .setDescription(`Translating "${input}" from ${origin} to ${target}...\n\nResult: ${res.text}`)
                     .setColor("#5865F2")
                     .setTimestamp()
                     .setFooter({
@@ -50,6 +57,7 @@ module.exports = {
                     })
 
                 await interaction.reply({ embeds: [translation_Embed] });
+
             } catch (error) {
                 client.log.error(error);
                 await interaction.reply({ content: "The API could not process this request", ephemeral: true });
