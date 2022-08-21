e673aa46ebf6e233e8ed9d32c42123f684ffbf31
julia <65985506+campanula@users.noreply.github.com>
2022-08-21 14:38:52 +0100

    Added: ready.js now chooses activities from array

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,16 +1,49 @@
+const { ActivityType } = require("discord.js");
+
 module.exports = {
     name: "ready",
     once: true,
-    execute(client) {
+    async execute(client) {
         client.log.botinfo(`${client.user.tag} is running!`);
-        client.user.setPresence({
-            status: "online",
-            activities: [
-                {
-                    name: "you",
-                    type: "WATCHING",
-                },
-            ],
-        });
+
+        const botStatus = [
+            {
+                status: "online",
+                name: "you",
+                type: ActivityType.Watching,
+            },
+            {
+                status: "online",
+                name: "Join Essex GDS!",
+                type: ActivityType.Playing,
+            },
+            {
+                status: "online",
+                name: "/help for commands",
+                type: ActivityType.Listening,
+            }
+        ];
+
+        client.choosePresence = async () => {
+            const choice = Math.floor(Math.random() * botStatus.length);
+            try {
+                await client.user.setPresence({
+                    activities: [
+                        {
+                            name: botStatus[choice].name,
+                            type: botStatus[choice].type,
+                        },
+                    ],
+                    status: botStatus[choice].status
+                });
+            } catch (error) {
+                client.error(error);
+                throw error;
+            }
+
+            return choice;
+        }
+        await client.choosePresence();
+        setInterval(client.choosePresence, 1000 * 60 * 15);
     },
 };
